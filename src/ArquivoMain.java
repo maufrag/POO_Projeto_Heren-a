a247b633a9bbe02d95a350c56aeda20cacf41f52
Mateus Torres Ferreira <34038838+maufrag@users.noreply.github.com>
2020-04-13 21:48:19 -0300

    Update
    Alteracao para versao com dados ja preenchidos. Versao para preenchimento em tempo de execucao ficará comentada

--- src/ArquivoMain.java
+++ src/ArquivoMain.java
@@ -11,7 +11,7 @@ public class ArquivoMain {
 		imprimirCabecalho();
 		
 		Scanner scan = new Scanner(System.in);
-		Funcionario funcionario = new Funcionario();
+		/*Funcionario funcionario = new Funcionario();
 		
 		System.out.println("Informe o nome do funcionario: ");
 		funcionario.setNome(scan.nextLine());
@@ -37,6 +37,34 @@ public class ArquivoMain {
 
 		System.out.println("Informe a quantidade de funcionarios: ");
 		gerente.setNumeroDeFuncionariosGerenciados(Integer.parseInt(scan.nextLine()));
+		
+		System.out.println("Informe a senha do gerente: ");
+		gerente.autenticar(Integer.parseInt(scan.nextLine()));
+		
+		gerente.listar();*/
+		
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Mateus");
+		
+		funcionario.setCpf("12345678910");
+		
+		funcionario.setSalario(1250.00);
+		
+		funcionario.listar();
+		
+		Gerente gerente = new Gerente();
+		
+		
+		gerente.setNome("Lucas");
+		
+		
+		gerente.setCpf("10987654321");
+		
+		
+		gerente.setSalario(1250.00);
+
+		
+		gerente.setNumeroDeFuncionariosGerenciados(1);
 		
 		System.out.println("Informe a senha do gerente: ");
 		gerente.autenticar(Integer.parseInt(scan.nextLine()));
